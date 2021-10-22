5c5fbbf85f46f4e804bc95b134b95322831edccb
Marcio <blogdomarcio@live.com>
2021-10-22 17:30:40 -0300

    Fix Index

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const express = require('express')
-
 const app = express()
-
-const sqlite = require('sqlite')
+const bodyParser = require('body-parser')
+const path = require('path');
+const { hostname } = require('os');
 
 const sqlite3 = require('sqlite3').verbose();
-
-const path = require('path')
-
-// const dbConnection = sqlite.open({
-//     filename: './banco.sqlite',
-//     driver: sqlite3.Database,
-// }, { Promise })
-
-// const dbConnection = sqlite.open({ filename: 'banco.sqlite', driver: sqlite3.Database }, { Promise })
-
-// const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
-
-// const dbConnection = sqlite.open('banco.sqlite', { Promise })
-
-
 const dbConnection = new sqlite3.Database(path.resolve(__dirname, 'banco.sqlite'), (err) => {
     if (err) {
         console.error(err.message);
@@ -27,207 +12,176 @@ const dbConnection = new sqlite3.Database(path.resolve(__dirname, 'banco.sqlite'
     //console.log('Connected to the database.');
 });
 
-app.set('views', path.resolve(__dirname, 'views'))
-
-app.set('view engine', 'ejs')
-
-app.use(express.static(path.resolve(__dirname, 'public')))
-
-app.use(express.urlencoded({ extended: true }))
+const port = process.env.PORT || 3000
 
 app.use('/admin', (req, res, next) => {
     if (req.hostname === 'localhost') {
         next()
     } else {
-        res.render('admin/semacesso')
+        res.send('Sorry, You Are Not Allowed to Access This Page')
     }
 })
 
-const port = process.env.PORT || 3000
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', async (resquest, response) => {
-
+app.get('/', async (req, res) => {
     const db = await dbConnection
-
-    const categoriasDb = await db.all('select * from categorias;')
-
-    const vagas = await db.all('select * from vagas;')
-
-    const categorias = categoriasDb.map(cat => {
-        return {
-            ...cat,
-            vagas: vagas.filter(vaga => vaga.categoria === cat.id)
-        }
+    const sqlCategorias = "SELECT * FROM categorias"
+    const sqlVagas = "SELECT * FROM vagas"
+    db.all(sqlCategorias, [], (err, categoriasDb) => {
+        db.all(sqlVagas, function (err, vagas) {
+            const categorias = categoriasDb.map(cat => {
+                return {
+                    ...cat,
+                    vagas: vagas.filter(vaga => vaga.categoria === cat.id)
+                }
+            })
+            res.render('home', {
+                categorias
+            })
+        })
     })
-
-    response.render('home', { categorias })
 })
 
 app.get('/vaga/:id', async (req, res) => {
-
     const db = await dbConnection
-    const vaga = await db.get('select * from vagas where id = ' + req.params.id)
-
-    // console.log(vaga)
-    // console.log(req.params)
-    res.render('vaga', {
-        vaga
-    })
+    const sql = "SELECT * FROM vagas WHERE id = ?";
+    db.get(sql, [req.params.id], (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.render("vaga", {
+            vaga: row
+        });
+    });
 })
 
-app.get('/admin', async (req, res) => {
+app.get('/admin', (req, res) => {
     res.render('admin/home')
 })
 
-app.get('/admin/departamentos', async (req, res) => {
-
-    const db = await dbConnection
-
-    const categorias = await db.all('select * from categorias;')
-
-
-    res.render('admin/departamentos', { categorias })
-})
-
-app.get('/admin/vagas', async (req, res) => {
-
-    const db = await dbConnection
-
-    const vagas = await db.all('select * from vagas;')
-
-    res.render('admin/vagas', { vagas })
+app.get('/admin/vagas', (req, res) => {
+    const db = dbConnection
+    const sql = "SELECT * FROM vagas"
+    db.all(sql, [], (err, results) => {
+        res.render("admin/vagas", {
+            results
+        });
+    });
 })
 
 
-app.get('/admin/vagas/excluir/:id', async (req, res) => {
-
+app.get('/admin/vagas/delete/:id', async (req, res) => {
     const db = await dbConnection
-
-    await db.run('delete from vagas where id =' + req.params.id + '')
-
+    await db.run('DELETE FROM vagas WHERE id = ' + req.params.id + '')
     res.redirect('/admin/vagas')
 })
 
+app.get('/admin/vagas/nova', async (req, res) => {
+    const db = dbConnection
+    const sql = "SELECT * FROM categorias"
+    db.all(sql, [], (err, categorias) => {
+        res.render("admin/nova-vaga", {
+            categorias
+        });
+    });
+})
 
-app.get('/admin/vagas/cadastrar/', async (req, res) => {
-
+app.post('/admin/vagas/nova', async (req, res) => {
     const db = await dbConnection
-
-    const categorias = await db.all('select * from categorias;')
-
-    res.render('admin/cadastro', { categorias })
+    const { titulo, descricao, categoria } = req.body
+    await db.run(`insert into vagas(categoria, titulo, descricao) values('${categoria}', '${titulo}', '${descricao}')`) //use temlate string (`)para adicionar variáveis à expressão sql
+    res.redirect('/admin/vagas')
 })
 
 app.get('/admin/vagas/editar/:id', async (req, res) => {
-
-    const db = await dbConnection
-
-    const categorias = await db.all('select * from categorias;')
-
-    const vaga = await db.get('select * from vagas where id =' + req.params.id + '')
-
-    res.render('admin/editar-vaga', { categorias, vaga })
+    const db = dbConnection
+    const { id } = req.params
+    const sqlCategorias = "SELECT * FROM categorias"
+    const sqlVagas = `SELECT * FROM vagas WHERE id='${id}'`
+    db.all(sqlCategorias, [], (err, categorias) => {
+        db.all(sqlVagas, function (err, vaga) {
+            res.render('admin/editar-vaga', {
+                categorias, vaga
+            })
+        })
+    })
 })
-
-app.get('/admin/departamento/editar/:id', async (req, res) => {
-
-    const db = await dbConnection
-
-    const categoria = await db.get('select * from categorias where id =' + req.params.id + '')
-
-    res.render('admin/editar-departamento', { categoria })
-})
-
 
 app.post('/admin/vagas/editar/:id', async (req, res) => {
-
     const db = await dbConnection
-
+    const { titulo, descricao, categoria } = req.body
     const { id } = req.params
-
-    const { categoria, titulo, descricao } = req.body
-
-    await db.run(`update vagas set categoria = '${categoria}', titulo = '${titulo}', descricao = '${descricao}' where id = ${id} `)
-
+    await db.run(`UPDATE vagas set categoria='${categoria}', titulo='${titulo}', descricao='${descricao}' WHERE id='${id}'`) //use temlate string (`)para adicionar variáveis à expressão sql
     res.redirect('/admin/vagas')
-
 })
 
+app.get('/admin/categorias', (req, res) => {
+    const db = dbConnection
+    const sql = "SELECT * FROM categorias"
+    db.all(sql, [], (err, categorias) => {
+        res.render("admin/categorias", {
+            categorias
+        });
+    });
+})
 
-app.post('/admin/departamento/editar/:id', async (req, res) => {
-
+app.get('/admin/categorias/delete/:id', async (req, res) => {
     const db = await dbConnection
+    await db.run('DELETE FROM categorias WHERE id = ' + req.params.id + '')
+    res.redirect('/admin/categorias')
+})
 
+app.get('/admin/categorias/nova', async (req, res) => {
+    res.render("admin/nova-categoria");
+})
+
+app.post('/admin/categorias/nova', async (req, res) => {
+    const db = await dbConnection
+    const { categoria } = req.body
+    await db.run(`insert into categorias(categoria) values('${categoria}')`) //use temlate string (`)para adicionar variáveis à expressão sql
+    res.redirect('/admin/categorias')
+})
+
+app.get('/admin/categorias/editar/:id', async (req, res) => {
+    const db = dbConnection
     const { id } = req.params
-
-    const { categoria } = req.body
-
-    await db.run(`update categorias set categoria = '${categoria}' where id = ${id} `)
-
-    res.redirect('/admin/departamentos')
-
-})
-
-app.get('/admin/departamento/cadastrar/', async (req, res) => {
-
-    res.render('admin/cadastroDepartamento')
-})
-
-app.post('/admin/departamento/cadastrar/', async (req, res) => {
-
-    const db = await dbConnection
-
-    const { categoria } = req.body
-
-    await db.run(`insert into categorias(categoria) values('${categoria}') `)
-
-    res.redirect('/admin/departamentos')
-})
-
-    .get('/admin/departamento/excluir/:id', async (req, res) => {
-
-        const db = await dbConnection
-
-        await db.run('delete from categorias where id =' + req.params.id + '')
-
-        res.redirect('/admin/departamentos')
+    const sql = `SELECT * FROM categorias WHERE id='${id}'`
+    db.all(sql, [], (err, categorias) => {
+        res.render('admin/editar-categoria', {
+            categorias
+        })
     })
-
-
-
-
-app.post('/admin/vagas/cadastrar/', async (req, res) => {
-
-    const db = await dbConnection
-
-    const { categoria, titulo, descricao } = req.body
-
-    await db.run(`insert into vagas(categoria, titulo, descricao) values('${categoria}', '${titulo}', '${descricao}') `)
-
-    res.redirect('/admin/vagas')
-
-
 })
 
+app.post('/admin/categorias/editar/:id', async (req, res) => {
+    const db = await dbConnection
+    const { categoria } = req.body
+    const { id } = req.params
+    await db.run(`UPDATE categorias set categoria='${categoria}' WHERE id='${id}'`) //use temlate string (`)para adicionar variáveis à expressão sql
+    res.redirect('/admin/categorias')
+})
 
 
 const init = async () => {
     const db = await dbConnection
     await db.run('create table if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT);')
     await db.run('create table if not exists vagas (id INTEGER PRIMARY KEY, categoria INTEGER, titulo TEXT, descricao TEXT);')
-    // const vaga = 'Marketing team (Remote)'
-    // const descricao = 'Vaga para Marketing team (Remote)'
-    // await db.run(`insert into vagas(categoria, titulo, descricao) values(1, '${vaga}', '${descricao}') `)
-    // await db.run(`insert into categorias(categoria) values('${categoria}')`)
+    // const categoria = 'Social Media'
+    // await db.run(`insert into categorias(categoria) values('${categoria}')`) 
+
+    //db.run("UPDATE vagas SET categoria = 3 WHERE (id = 3);");
 }
 init()
 
+
 app.listen(port, (err) => {
     if (err) {
-        console.log('Erro ao iniciar servidor')
+        console.log('Não foi possível iniciar o servidor')
     } else {
-        console.log('Servidor Jobify iniciado com sucesso - @blogdomarcio')
+        console.log('O servidor Jobify está online!')
     }
-}
-
-)
+})
